616c1a680fca44d6cf3e01aaf6518560d65b0b7f
wikosip <wiklaurirati8@gmail.com>
2025-03-10 22:50:42 +0400

    registration modified

---
+++ Todo_List_API/Registration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_List_API
+{
+    internal class Registration : User
+    {
+        public Registration(string name, string email, string password) : base (name, email, password)
+        {
+            
+        }
+
+        public string RegistrationUsername(string username)
+        {
+            throw new ArgumentException();
+        }
+
+        public string UsernameExist()
+        {
+            throw new ArgumentException();
+        }
+    }
+}
